95bf0de759c5a32191c56c06af02ae67d217e11d
Ali95800 <ali.c95800@gmail.com>
2021-12-22 16:51:15 +0100

    js avc j-c

---
+++ js/01_dom.js
@@ -0,0 +1,35 @@
+/**
+
+ * header avc 1 h1 centré
+
+ *
+
+ * main avc 2 sections
+
+ * 1 titre pr chaque section
+
+ * dns chaque section 3 paragraph
+
+ * au moin 1 img dns la section
+
+ *
+
+ * footer : mention legale et confidentialité 2 liens <a> centré
+
+ * changer d'img en JS
+
+ */
+let selectBody = document.getElementsByTagName("body");
+console.log(selectBody);
+selectBody[0].style.background = "red";
+
+let selectParagraphe = document.getElementsByClassName("paragraphe");
+console.log(selectParagraphe);
+selectParagraphe[0].style.color = "white";
+
+let selectImg = document.querySelectorAll("img");
+console.log(selectImg);
+
+for (let i = 0; i < selectImg.length; i++) {
+  selectImg[2].src = "img/renji.jpg";
+}
---
+++ js/evenement.js
@@ -0,0 +1,83 @@
+"use strict";
+/**
+ * EN HAUT MES VARIABLES
+ *
+ */
+let questionContainer = document.querySelector(".click_event");
+console.log(questionContainer);
+//pour cibler le bouton 1
+let btn1 = document.querySelector("#btn-1");
+console.log(btn1);
+//pour cibler le bouton 2
+let btn2 = document.querySelector("#btn-2");
+console.log(btn2);
+//la reponse
+let reponse = document.querySelector(".reponse");
+console.log(reponse);
+let reponseF = document.querySelector(".reponseF");
+console.log(reponseF);
+let inputId = document.querySelector("#inputId");
+console.log(inputId.value);
+//pour le mousemove
+let mouseMove = document.querySelector("#mouseMove");
+console.log(mouseMove);
+/**
+ *
+ *
+ *
+ */
+// j'écoute l'action du user sur la boite de ma question
+questionContainer.addEventListener("click", function () {
+  console.log("je clcik sur la boite de ma question");
+});
+// questionContainer.addEventListener("click", () => {
+//   //mon code ici
+// });
+
+// je crée une fonction
+function maFonction() {
+  console.log("ma fonction est click");
+}
+// je déclenche ma fonction quand le user click sur la boite de la question
+questionContainer.addEventListener("click", maFonction);
+
+btn1.addEventListener("click", function () {
+  reponse.style.visibility = "hidden";
+  reponse.style.display = "none";
+  reponseF.style.display = "block";
+  reponseF.style.visibility = "visible";
+  questionContainer.style.background = "red";
+});
+
+btn2.addEventListener("click", function () {
+  reponseF.style.visibility = "hidden";
+  reponse.style.display = "block";
+  reponseF.style.display = "none";
+  reponse.style.visibility = "visible";
+  questionContainer.style.background = "green";
+});
+
+// MOUSEMOVE
+// event = e
+window.addEventListener("mousemove", function (event) {
+  //on  récupère l'event
+  console.log(event);
+  mouseMove.style.left = event.pageX + "px";
+  mouseMove.style.top = event.pageY + "px";
+});
+
+//MOUSEUP MOUSEDOWN
+window.addEventListener("mousedown", () => {
+  mouseMove.style.transform = "scale(2) translate(-25%, -25%)";
+});
+window.addEventListener("mouseup", () => {
+  mouseMove.style.transform = "scale(1) translate(-50%, -50%)";
+});
+
+// inputId.value = "toto";
+
+// inputId.addEventListener("input", function () {
+//   console.log(inputId.value);
+//   let result = parseInt(inputId.value);
+//   console.log(result + 2);
+// });
